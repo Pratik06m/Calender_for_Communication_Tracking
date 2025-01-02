2d982944d9ec2056b112f91323b8b9f7d58b242a
Harshal Puri <hrp3102002@gmail.com>
2025-01-02 13:48:21 +0530

    Api Integration for login Admin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -177,7 +177,7 @@ function AdminForm() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://example.com/api/admin/login", {
+      const response = await fetch("http://localhost:4000/api/v1/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
